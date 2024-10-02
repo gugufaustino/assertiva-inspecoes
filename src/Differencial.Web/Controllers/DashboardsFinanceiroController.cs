c86695fe628064b8a6dc6033bf2134eed69cff70
Wellington Faustino <well.faustino@hotmail.com>
2024-10-01 22:40:14 -0300

    Refactor DashboardsFinanceiroController routes and methods

--- src/Differencial.Web/Controllers/DashboardsFinanceiroController.cs
+++ src/Differencial.Web/Controllers/DashboardsFinanceiroController.cs
@@ -1,9 +1,8 @@
-﻿using Differencial.Domain.Contracts.Infra;
-using Differencial.Domain.Contracts.Repositories;
+﻿using Differencial.Domain.Contracts.Repositories;
 using Differencial.Domain.Contracts.Services;
+using Differencial.Domain.Entities;
 using Differencial.Domain.UOW;
 using Differencial.Domain.Util.ExtensionMethods;
-using Differencial.Service.Services;
 using Differencial.Web.Controllers;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -13,7 +12,7 @@ using System.Threading.Tasks;
 namespace WEB.Controllers
 {
 
-	[Route("Dashboards/FinanceiroReceber")]
+    [Route("Dashboards")]
 	public class DashboardsFinanceiroController : BaseController
 	{ 
 		private readonly ISeguradoraService _seguradoraService; 
@@ -33,7 +32,8 @@ namespace WEB.Controllers
 
 		}
 
-		[HttpGet("")]
+        #region FinanceiroReceber
+        [HttpGet("FinanceiroReceber")]
 		public IActionResult FinanceiroReceber()
 		{
 			var selectList = GerarCompetencias(new DateTime(2023, 1, 1), DateTime.Now.AddMonths(1));
@@ -42,7 +42,7 @@ namespace WEB.Controllers
 			return View();
 		}
 
-		[HttpPost("ListarDadosFinanceiroReceber")]
+        [HttpPost("FinanceiroReceber/ListarDadosFinanceiroReceber")]
 		public IActionResult ListarDadosFinanceiroReceber(string mesano)
 		{
 			var mes = int.Parse(mesano.Split('/')[0]);
@@ -52,8 +52,7 @@ namespace WEB.Controllers
 			return ResponseResult(true, content: lstSolicitacao);
 		}
 
-
-		[HttpGet("ReceberLancamentos")]
+        [HttpGet("FinanceiroReceber/ReceberLancamentos")]
 		public ActionResult ReceberLancamentos(int id, string mesano)
 		{
 			var mes = int.Parse(mesano.Split('/')[0]);
@@ -66,41 +65,9 @@ namespace WEB.Controllers
 			return View("ReceberLancamentos", lstSolicitacao);
 		}
 
-		//public ActionResult FinanceiroPagar()
-		//{
-		//    var lstSolicitacao = _dashboardsService.ListarSolicitacoesFinanceiro();
-
-		//    return View(lstSolicitacao);
-		//}
-
-		//[HttpGet]
-		//public ActionResult Faturar(int Id)
-		//{
-		//    return View("Financeiro/Faturar");
-		//}
 
 
-		private static List<KeyValuePair<string, string>> GerarCompetencias(DateTime dataInicio, DateTime dataFim)
-		{
-			var listaCompetencias = new List<KeyValuePair<string, string>>();
-
-			var dataAtual = dataFim;
-
-			while (dataAtual >= dataInicio)
-			{
-				var key = $"{dataAtual.Month}/{dataAtual.Year}";
-				var value = $"{dataAtual.ToString("MMMM", new System.Globalization.CultureInfo("pt-BR"))}/{dataAtual.Year}";
-
-				listaCompetencias.Add(new KeyValuePair<string, string>(key, value));
-
-				dataAtual = dataAtual.AddMonths(-1);
-			}
-
-			return listaCompetencias;
-		}
-
-
-		[HttpPost("Faturar")]
+        [HttpPost("FinanceiroReceber/Faturar")]
 		[ServiceFilter(typeof(TransactionFilter))]
 		public async Task<JsonResult> Faturar(int[] Id, string mesano)
 		{
@@ -112,7 +79,7 @@ namespace WEB.Controllers
 			return ResponseResult(true);
 		}
 
-		[HttpPost("Liquidar")]
+        [HttpPost("FinanceiroReceber/Liquidar")]
 		[ServiceFilter(typeof(TransactionFilter))]
 		public async Task<JsonResult> Liquidar(int[] Id, string mesano)
 		{
@@ -123,5 +90,54 @@ namespace WEB.Controllers
 			await AppSaveChangesAsync();
 			return ResponseResult(true);
 		}
+
+        #endregion
+
+
+        #region FinanceiroPagar
+        [HttpGet("FinanceiroPagar")]
+        public IActionResult FinanceiroPagar()
+        {
+            var selectList = GerarCompetencias(new DateTime(2023, 1, 1), DateTime.Now.AddMonths(1));
+            ViewData["competenciaMes"] = selectList.ToSelectList(i => i.Key, i => i.Key, false);
+           
+            return View();
+
+        }
+
+		[HttpPost("FinanceiroPagar/ListarDadosFinanceiro")]
+		public IActionResult ListarDadosFinanceiroPagar(string mesano)
+		{
+			var mes = int.Parse(mesano.Split('/')[0]);
+			var ano = int.Parse(mesano.Split('/')[1]);
+
+			var lstSolicitacao = _lancamentoFinanceiroTotalRepository.FinanceiroPagar(ano, mes);
+			return ResponseResult(true, content: lstSolicitacao);
+		}
+
+		#endregion
+
+		#region Aux
+
+		private static List<KeyValuePair<string, string>> GerarCompetencias(DateTime dataInicio, DateTime dataFim)
+        {
+            var listaCompetencias = new List<KeyValuePair<string, string>>();
+
+            var dataAtual = dataFim;
+
+            while (dataAtual >= dataInicio)
+            {
+                var key = $"{dataAtual.Month}/{dataAtual.Year}";
+                var value = $"{dataAtual.ToString("MMMM", new System.Globalization.CultureInfo("pt-BR"))}/{dataAtual.Year}";
+
+                listaCompetencias.Add(new KeyValuePair<string, string>(key, value));
+
+                dataAtual = dataAtual.AddMonths(-1);
+            }
+
+            return listaCompetencias;
+        }
+
+        #endregion
 	}
 }
