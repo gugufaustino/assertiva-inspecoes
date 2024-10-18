c309d949de86c6b70c0368e91874a5916497f871
Wellington Faustino <well.faustino@hotmail.com>
2024-10-18 15:37:59 -0300

    Secrets

--- src/Differencial.Web/Startup.cs
+++ src/Differencial.Web/Startup.cs
@@ -18,121 +18,126 @@ using System.IO;
 
 namespace Differencial.Web
 {
-    public class Startup
-    {
-        public IConfiguration Configuration { get; }
-        public IHostEnvironment HostEnvironment { get; }
+	public class Startup
+	{
+		public IConfiguration Configuration { get; }
+		public IHostEnvironment HostEnvironment { get; }
 
-        public Startup(IHostEnvironment hostingEnvironment)
-        {
-           
-            var builder = new ConfigurationBuilder()
-                               .SetBasePath(hostingEnvironment.ContentRootPath)
-                               .AddJsonFile("appsettings.json", true, true)
-                               .AddJsonFile($"appsettings.{hostingEnvironment.EnvironmentName}.json", true, true)
-                               .AddEnvironmentVariables();
+		public Startup(IHostEnvironment hostingEnvironment)
+		{
 
-            this.Configuration = builder.Build();
-            this.HostEnvironment = hostingEnvironment;
-        }
+			var builder = new ConfigurationBuilder()
+							   .SetBasePath(hostingEnvironment.ContentRootPath)
+							   .AddJsonFile("appsettings.json", true, true)
+							   .AddJsonFile($"appsettings.{hostingEnvironment.EnvironmentName}.json", true, true)
+							   .AddEnvironmentVariables();
 
+			if (hostingEnvironment.IsDevelopment())
+				builder.AddUserSecrets<Startup>();
 
-        public void ConfigureServices(IServiceCollection services)
-        {
-            var mvcBuilder = services.AddControllersWithViews(ConfigureMvcOptions)                
-                                    .AddNewtonsoftJson(options =>
-                                    {
-                                        options.UseMemberCasing();
-                                        options.SerializerSettings.Culture = new System.Globalization.CultureInfo("pt-BR");
-                                        options.SerializerSettings.Converters.Add(new Helpers.DecimalDoubleJsonConverter());
-                                        // Converters = new List<JsonConverter> { new DecimalDoubleConverter() }
-                                    });
-
-            if (HostEnvironment.IsDevelopment())
-            { 
-                mvcBuilder.AddRazorRuntimeCompilation();
-            }
-
-            services.AddLogging(builder => builder.AddConsole());
-            services.AddSingleton<IFileProvider>(new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot")));
+			this.Configuration = builder.Build();
+			this.HostEnvironment = hostingEnvironment;
+		}
 
 
+		public void ConfigureServices(IServiceCollection services)
+		{
+			var mvcBuilder = services.AddControllersWithViews(ConfigureMvcOptions)
+									.AddNewtonsoftJson(options =>
+									{
+										options.UseMemberCasing();
+										options.SerializerSettings.Culture = new System.Globalization.CultureInfo("pt-BR");
+										options.SerializerSettings.Converters.Add(new Helpers.DecimalDoubleJsonConverter());
+										// Converters = new List<JsonConverter> { new DecimalDoubleConverter() }
+									});
+
+			if (HostEnvironment.IsDevelopment())
+			{
+				mvcBuilder.AddRazorRuntimeCompilation();
+			}
+
+			services.AddLogging(builder => builder.AddConsole());
+			services.AddSingleton<IFileProvider>(new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot")));
 
 
-            services.AddDbContext<DifferencialContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DifferencialConnection"))
-                                                                                        .EnableSensitiveDataLogging()  // Para incluir dados dos parâmetros na query gerada
-                                                                                           .LogTo(Console.WriteLine, Microsoft.Extensions.Logging.LogLevel.Information)); // Nível do log);
 
-            //services.AddAutoMapper(typeof(Startup));
-            Infra.AutoMapperConfig.RegisterAutoMapper();
 
-            services.ResolveDependencies();
+			services.AddDbContext<DifferencialContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DifferencialConnection"))
+																						.EnableSensitiveDataLogging()  // Para incluir dados dos parâmetros na query gerada
+																						   .LogTo(Console.WriteLine, Microsoft.Extensions.Logging.LogLevel.Information)); // Nível do log);
 
-            services.AddDistributedMemoryCache();
-            services.AddSession(options =>
-            {
-                options.IdleTimeout = TimeSpan.FromSeconds(1200);
-                options.Cookie.HttpOnly = true;
-                options.Cookie.IsEssential = true;
-            });
-            services.AddSignalR(i=>
-            {
-                i.EnableDetailedErrors = true;
-                i.MaximumReceiveMessageSize = long.MaxValue;
-            });
+			//services.AddAutoMapper(typeof(Startup));
+			Infra.AutoMapperConfig.RegisterAutoMapper();
 
-            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(options => {
-                options.ExpireTimeSpan = TimeSpan.FromMinutes(20);
-                options.SlidingExpiration = true;
-                options.AccessDeniedPath = "/Error401/";
-                options.LoginPath = "/Home/Login/";
-            });
-            
+			services.ResolveDependencies();
 
-        }
+			services.AddDistributedMemoryCache();
+			services.AddSession(options =>
+			{
+				options.IdleTimeout = TimeSpan.FromSeconds(1200);
+				options.Cookie.HttpOnly = true;
+				options.Cookie.IsEssential = true;
+			});
+			services.AddSignalR(i =>
+			{
+				i.EnableDetailedErrors = true;
+				i.MaximumReceiveMessageSize = long.MaxValue;
+			});
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
+			services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(options =>
+			{
+				options.ExpireTimeSpan = TimeSpan.FromMinutes(20);
+				options.SlidingExpiration = true;
+				options.AccessDeniedPath = "/Error401/";
+				options.LoginPath = "/Home/Login/";
+			});
 
-            using (var scope = app.ApplicationServices.CreateScope())
-            {
-                var dbContext = scope.ServiceProvider.GetRequiredService<DifferencialContext>();
-                dbContext.Database.Migrate(); // Aplica as migrations automaticamente
-            } 
 
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else {
-                //app.UseExceptionHandler("/Home/Error");
-                app.UseDeveloperExceptionPage(); //TODO melhorar isso
-                app.UseHsts();
-            }
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-            
-            app.UseRouting();
-            app.UseAuthentication();
-            app.UseAuthorization();
+		}
 
-            app.UseSession();
-            app.UseGlobalizationConfig();
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-                
-                endpoints.MapHub<NotificationHub>("/hubs");
-                endpoints.MapControllers().RequireAuthorization();
-            });
-        }
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+		{
 
-        private void ConfigureMvcOptions(MvcOptions mvcOptions)
-        {
-            mvcOptions.ModelBinderProviders.Insert(0, new DecimalModelBinderProvider());
-        }
+			using (var scope = app.ApplicationServices.CreateScope())
+			{
+				var dbContext = scope.ServiceProvider.GetRequiredService<DifferencialContext>();
+				dbContext.Database.Migrate(); // Aplica as migrations automaticamente
+			}
 
-    }
+			if (env.IsDevelopment())
+			{
+				app.UseDeveloperExceptionPage();
+			}
+			else
+			{
+				//app.UseExceptionHandler("/Home/Error");
+				app.UseDeveloperExceptionPage(); //TODO melhorar isso
+				app.UseHsts();
+			}
+			app.UseHttpsRedirection();
+			app.UseStaticFiles();
+
+			app.UseRouting();
+			app.UseAuthentication();
+			app.UseAuthorization();
+
+			app.UseSession();
+			app.UseGlobalizationConfig();
+			app.UseEndpoints(endpoints =>
+			{
+				endpoints.MapControllerRoute(
+					name: "default",
+					pattern: "{controller=Home}/{action=Index}/{id?}");
+
+				endpoints.MapHub<NotificationHub>("/hubs");
+				endpoints.MapControllers().RequireAuthorization();
+			});
+		}
+
+		private void ConfigureMvcOptions(MvcOptions mvcOptions)
+		{
+			mvcOptions.ModelBinderProviders.Insert(0, new DecimalModelBinderProvider());
+		}
+
+	}
 }
